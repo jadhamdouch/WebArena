35aa07d6b404e527aef43f43bfd3c19e7615da90
Jad Hamdouch <jad.hamdouch@edu.ece.fr>
2017-10-24 13:08:41 +0200

    Formulaire création user fonctionel

--- src/Model/Table/PlayersTable.php
+++ src/Model/Table/PlayersTable.php
@@ -15,4 +15,25 @@ class PlayersTable extends Table
             return false;
         }    
     }
+    
+    public function createPlayer($name,$pass){
+        $player = $this->newEntity();
+        
+        $player-> email = $name;
+        $player-> password = $pass;
+        
+        if($this->save($player)){
+            $id = $player -> id ;
+        }
+        
+    }
+    
+    public function createPlayerC($name,$pass){
+        $query = $this->query();
+        $query -> insert(['email','password'])
+                ->values(['email'=>$name,
+                    'password' => $pass])
+                ->execute();
+        $id =$this->id ;
+    }
 }
