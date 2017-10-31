703edabbd64a9b3cfd9331c26d8be26f04968bd3
Jad Hamdouch <jad.hamdouch@edu.ece.fr>
2017-10-31 18:05:49 +0100

    tools

--- src/Model/Table/ToolsTable.php
+++ src/Model/Table/ToolsTable.php
@@ -8,18 +8,17 @@ class ToolsTable extends Table
     
     
     public function createTools(){
-        $this->loadModel('Fighters');
-        $tools[] = array('D','L','V');
-        $nb =1;
+        
+        $tools = array("D","L","V");
         for ($i = 0 ; $i<10;$i++){
-            $rand_keys = array_rand($tools, 1);
+            $nb =1 ;
             $tool = $this->newEntity();
-            $tool -> type = $tools[$rand_keys];
+            $tool -> type = $tools[rand(0,2)];
             $tool -> bonus = rand(1,2);
             while($nb != 0){
                $y = rand(1,10);
                $x= rand(1,15);
-               $test = $this->Fighters->find('all',array('conditions' => array('coordinate_y' => $y,'coordinate_x' => $x)));
+               $test = $this->find('all',array('conditions' => array('coordinate_y' => $y,'coordinate_x' => $x)));
                 $nb = $test -> count();
             }
             $tool -> coordinate_x = $x;
